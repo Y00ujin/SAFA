f71635b5b3482f8158816d27a14a7fc226015d0a
Y00ujin <yujingim43@gmail.com>
2022-08-21 20:47:53 +0900

    create: SAFA Project 생성

---
+++ SAFA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SAFA
+//
+//  Created by 김유진 on 2022/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
